fa1c7d0cf6275b5520aa1a08d2685f115320b409
emanu <emanu@172.29.106.22>
2026-06-22 18:29:41 -0300

    feat: cria classe base Cliente e inicializa saldo de XP

--- src/br/edu/cafeteria/modelo/Cliente.java
+++ src/br/edu/cafeteria/modelo/Cliente.java
@@ -1,19 +1,21 @@
 package br.edu.cafeteria.modelo;
 
 public abstract class Cliente {
-
     private String nome;
     private String cpf;
-    protected double saldoXP; 
-    public Cliente(String nome, String cpf) {
+    private double saldoXP;
+
+    public Cliente(String nome, String cpf, double saldoXP) {
         this.nome = nome;
         this.cpf = cpf;
-        this.saldoXP = 0.0; 
+        this.saldoXP = saldoXP;
     }
 
-    public abstract void adicionarXP(double valorGasto);
+    public abstract double calcularXPGanhado(double valorGasto);
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
     public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
     public double getSaldoXP() { return saldoXP; }
+    public void setSaldoXP(double saldoXP) { this.saldoXP = saldoXP; }
 }
