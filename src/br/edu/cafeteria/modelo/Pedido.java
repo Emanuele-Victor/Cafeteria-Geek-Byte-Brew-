0d50a16a5545e04c0124ae559ec47da989f4d72a
Denilson Lima <denilson.lima04@outlook.com>
2026-06-23 20:25:32 -0300

    feat: adiciona controle de estoque e lanca EstoqueInsuficienteException

--- src/br/edu/cafeteria/modelo/Pedido.java
+++ src/br/edu/cafeteria/modelo/Pedido.java
@@ -2,11 +2,10 @@ package br.edu.cafeteria.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import br.edu.cafeteria.excecao.EstoqueInsuficienteException;
 
 public class Pedido {
-    
     private static int contadorSequencial = 1;
-
     private int id;
     private List<ItemPedido> itens;
 
@@ -15,20 +14,21 @@ public class Pedido {
         this.itens = new ArrayList<>();
     }
 
-    public int getId() {
-        return id;
+    public int getId() { return id; }
+    public List<ItemPedido> getItens() { return itens; }
+
+    public void adicionarItem(Produto p) throws EstoqueInsuficienteException {
+        this.adicionarItem(p, 1);
     }
 
-    public List<ItemPedido> getItens() {
-        return itens;
-    }
-
-    public void adicionarItem(Produto p) {
-       this.adicionarItem(p, 1);
-    }
- 
-    public void adicionarItem(Produto p, int quantidade) {        
-        ItemPedido novoItem = new ItemPedido(p, quantidade);        
+    public void adicionarItem(Produto p, int quantidade) throws EstoqueInsuficienteException {
+        if (quantidade > p.getQuantidadeEstoque()) {
+            throw new EstoqueInsuficienteException("Estoque insuficiente para o produto: " + p.getNome() + " (Disponível: " + p.getQuantidadeEstoque() + ")");
+        }
+        
+        ItemPedido novoItem = new ItemPedido(p, quantidade);
         this.itens.add(novoItem);
+        
+        p.setQuantidadeEstoque(p.getQuantidadeEstoque() - quantidade);
     }
 }
