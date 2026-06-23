340676ddf5f4edd86e47f5b3351c9d0a03903757
VictorHugoCoel <victorcoelho1608@hotmail.com>
2026-06-23 09:02:39 -0300

    feat: cria classe ItemPedido associando Produto e quantidade

---
+++ src/br/edu/cafeteria/modelo/ItemPedido.java
@@ -0,0 +1,28 @@
+package br.edu.cafeteria.modelo;
+
+public class ItemPedido {
+    
+    private Produto produto;
+    private int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularSubtotal() {
+        return produto.getPrecoBase() * quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
